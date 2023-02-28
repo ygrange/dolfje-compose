aa2eaf8b3f8458d331065b80824c2d71ba18e19f
Yan <12-grange@users.noreply.git.astron.nl>
2023-02-28 11:23:23 +0000

    Fix auto timestamping

--- dolfdb/init_databasecontent.sql
+++ dolfdb/init_databasecontent.sql
@@ -1,5 +1,4 @@
 CREATE USER 'pinger'@'localhost' ;
 GRANT usage on *.* to 'pinger'@'localhost';
 
-INSERT INTO games (gms_name, gms_status, gms_vote_style, gms_revive) values ('ww0', 'ENDED', 'blind', 0);
-
+INSERT INTO games (gms_name, gms_created_at) values ('ww0',NULL);
